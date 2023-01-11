32aab78586ed7bb4530d6d19b026a8a9bcd0d455
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2023-01-11 08:49:04 -0600

    rotar video
    adjunto la función de rotar video para responsive

--- vistas/js/rutas.js
+++ vistas/js/rutas.js
@@ -2,6 +2,10 @@ function mostrarRuta(laboratorio){
 
  $('#video-overlay').addClass('open');
  $("#video-overlay").append('<video width="820" height="480" autoplay loop> <source src="/TEC-LANDIVAR-MAPA/vistas/js/'+laboratorio+'.mp4" type="video/mp4"> <p>Your browser doesnt support HTML5 video.</p> </video>');
+ $("#video-overlay").append('<div class="rotate-phone-please">'+
+ '<div class="phone"></div>'+  
+ '<div class="message"><p>Porfavor rota tu teléfono 90°</p></div>'+
+'</div>');
 }
 
 $('.video-overlay, .video-overlay-close').on('click', function(e){
